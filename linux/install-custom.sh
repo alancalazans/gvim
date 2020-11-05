ab9dcb988c4b55d46df1fe55a9f480f07bd7d7a0
alancalazans <alan2calazans@gmail.com>
2020-11-05 19:36:55 -0300

    05/11/2020 19:36

--- linux/install-custom.sh
+++ linux/install-custom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp linux.vimrc ~/.vimrc
+cp linux.vimrc $HOME/.vimrc
+tar -Jxvf linux.vim.tar.xz -C $HOME/
 sudo cp linux.vimrc /root/.vimrc
-tar -Jxvf linux.vim.tar.xz -C ~/
 sudo tar -Jxvf linux.vim.tar.xz -C /root/

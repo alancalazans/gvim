668526f304774e2c617d59d466ccdafe2deae23a
alancalazans <alan2calazans@gmail.com>
2023-06-11 21:17:50 -0300

    2023.06.11 21:17

--- extract-customize.sh
+++ extract-customize.sh
@@ -3,16 +3,22 @@
 unzip _vim.zip -d $HOME/
 cp snippets/* $HOME/.vim/snippets/
 cp _vimrc $HOME/.vimrc
-mkdir $HOME/.local/share/fonts
-cp NeoVim_fonts/Droid*.otf $HOME/.local/share/fonts/
-unzip NeoVim_fonts/FiraMono.zip -d $HOME/.local/share/fonts/
+# Caso o diretório de fontes não exista.
+if ! [ -d "$HOME/.local/share/fonts" ]; then
+	mkdir -p "$HOME/.local/share/fonts"
+	cp NeoVim_fonts/Droid*.otf "$HOME/.local/share/fonts/"
+	unzip NeoVim_fonts/FiraMono.zip -d "$HOME/.local/share/fonts/"
+fi
 #root
 sudo unzip _vim.zip -d /root/
 sudo cp snippets/* /root/.vim/snippets/
 sudo cp _vimrc /root/.vimrc
-sudo mkdir -p /root/.local/share/fonts
-sudo cp NeoVim_fonts/Droid*.otf /root/.local/share/fonts/
-sudo unzip NeoVim_fonts/FiraMono.zip -d /root/.local/share/fonts/
+# Caso o diretório de fontes não exista.
+if ! [ -d "/root/.local/share/fonts" ]; then
+	sudo mkdir -p "/root/.local/share/fonts"
+	sudo cp NeoVim_fonts/Droid*.otf "/root/.local/share/fonts"
+	sudo unzip NeoVim_fonts/FiraMono.zip -d "/root/.local/share/fonts"
+fi
 #sudo mkdir -p /opt/omnisharp
 #sudo tar -zxf Omnisharp-Roslyn-Server/omnisharp-linux-x64-net6.0.tar.gz -C /opt/omnisharp/
 #--------------------
@@ -25,21 +31,23 @@ npm install eslint -g
 # Criando arquivo de configuração
 # `.eslintrc.js` na pasta de usuário:
 #--------------------
-echo -e 'module.exports = {
-    "env": {
-      "browser": true,
-      "es2021": true
-    },
-    "overrides": [
-    ],
-    extends: [
-      "standard",
-      "eslint:recommended",
-      "prettier"
-    ],
-    "parserOptions": {
-      "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}' > ~/.eslintrc.js
+if [ ! -f ~/.eslintrc.js ]; then
+	echo -e 'module.exports = {
+			"env": {
+				"browser": true,
+				"es2021": true
+			},
+			"overrides": [
+			],
+			extends: [
+				"standard",
+				"eslint:recommended",
+				"prettier"
+			],
+			"parserOptions": {
+				"ecmaVersion": "latest"
+			},
+			"rules": {
+			}
+	}' > ~/.eslintrc.js
+fi
